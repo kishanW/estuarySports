0bf96e96dd93b6d944f1d4868e0b7cc88c0dc54c
kishanW <anatoxind@gmail.com>
2013-11-19 22:50:44 -0500

    FN002 added suevafree theme

---
+++ wp-content/themes/suevafree/core/post-formats/audio.php
@@ -0,0 +1,61 @@
+<?php 
+	
+	global $suevafree_wip_setting;
+    
+	if ( has_post_thumbnail() ) : ?>
+        
+		<div class="pin-container">
+			<?php the_post_thumbnail('blog'); ?>
+        </div>
+        
+<?php 
+
+	endif; 
+	
+?>
+    
+<article class="article">
+
+    <h1 class="title"><a href="<?php echo get_permalink($post->ID); ?>"><?php the_title(); ?></a></h1>
+    
+    <div class="line"> 
+
+        <div class="entry-info <?php if (suevafree_setting('suevafree_view_comments') == "on" ) echo "viewcomments"; ?>">
+       
+            <span class="entry-date"><i class="icon-time" ></i><?php echo get_the_date(); ?></span>
+            <?php if (suevafree_setting('suevafree_view_comments') == "on" ): ?>
+            <span class="entry-comments"><i class="icon-comments-alt" ></i><?php echo comments_number( '<a href="'.get_permalink($post->ID).'#respond">'.__( "No comments","wip").'</a>', '<a href="'.get_permalink($post->ID).'#comments">1 '.__( "comment","wip").'</a>', '<a href="'.get_permalink($post->ID).'#comments">% '.__( "comments","wip").'</a>' ); ?></span>
+            <?php endif; ?>
+            <span class="entry-standard"><i class="icon-music"></i><?php _e( "Audio","wip") ?></span>
+        </div>
+    
+    </div>
+
+	<?php 
+	
+	if ((is_home()) || (is_category()) || (is_page() )):
+		
+		the_excerpt(); 
+	
+	else:
+
+		the_content();
+		
+		echo "<div class='clear'></div>";
+		
+		wp_link_pages();
+
+		echo '<p><strong>'. __( "Categories: ","wip").'</strong>'; the_category(', '); echo '</p>';
+		
+		the_tags( '<footer class="line"><div class="entry-info"><span class="tags">Tags: ', ', ', '</span></div></footer>' );
+		
+		if (suevafree_setting('suevafree_view_comments') == "on" ) :
+			comments_template();
+		endif;
+		
+	endif;
+	
+	
+	?>
+
+</article>
